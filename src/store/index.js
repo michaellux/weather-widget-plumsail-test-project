017fdd04351e22a929a30f99a139ee4ded07097e
michaellux <michaellux@yandex.ua>
2021-01-20 12:41:10 +0300

    create primary structure of widget store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import cities from "./modules/cities";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {
+    cities
+  }
 });
